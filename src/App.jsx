e57a856f7252af5f064211c18467313780b26290
Cloud Odhiambo Nyang'wera <cloudodhiambo2@gmail.com>
2025-04-24 13:18:16 +0300

    Resolves error in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-=======
+
 import { BrowserRouter } from 'react-router-dom';
-import Footer from "./components/footer.jsx"
-import './App.css';
 import AppRoutes from './routes';
-import NavBar from './components/NavBar';
+import NavBar from './components/navBar.jsx';
 import Footer from "./components/footer.jsx"
 
 function App() {
@@ -11,7 +9,7 @@ function App() {
     <BrowserRouter>
       <NavBar />
       <AppRoutes />
-    <Footer/>
+      <Footer/>
     </BrowserRouter>
   );
 }
