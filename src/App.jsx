3bf39f5d2c9adecfab5b5d22970932ddde116fd2
Arnold Wainaina <arnoldwainaina2865@gmail.com>
2025-04-24 15:41:24 +0300

    updated files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Products from "./components/Products";
+
+function App() {
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <h1 className="text-4xl font-bold text-center py-8">🪑 OfficeFit Product Showcase</h1>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
--- src/Pages/Cabinets.jsx
+++
@@ -1,5 +0,0 @@
-// Cabinets.jsx
-export default function Cabinets() {
-    return <h1>Cabinets Page</h1>;
-  }
-  
